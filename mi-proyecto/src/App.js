16d01953c5b7e77ccbba8ffd42d84e664db90e0b
jesus2419 <102651007+jesus2419@users.noreply.github.com>
2024-02-27 20:31:25 -0600

    registro inicio
    Creación del forms para el registro

---
+++ mi-proyecto/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Formulario from './Registro';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Formulario />
+  </div>
+
+  );
+}
+
+export default App;
---
+++ mi-proyecto/src/Registro.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const Formulario = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    // Aquí puedes enviar los datos a un servidor o hacer lo que necesites con ellos
+  };
+
+  return (
+    <div>
+      <h2>Formulario de Inicio de Sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Nombre de Usuario:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
